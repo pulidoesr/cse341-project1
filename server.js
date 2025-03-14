78ced09c0b34baf80ebfce52824d10dd51eded1f
Eduardo Pulido <pul21010@byui.edu>
2025-03-13 23:43:43 -0500

    W02 Update Server with Cors

--- server.js
+++ server.js
@@ -22,6 +22,15 @@ app.use(express.static(path.join(__dirname, './cse341-project1')));
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+  });
 
 
 app.use('/', contactsRoute); 
